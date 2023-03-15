316cf4ee787cee2dbfcea1d7a91c2657331e879e
unik0 <murotkafbi@gmail.com>
2023-03-15 19:31:26 +0300

    ooow2.text

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car  extends  WheeledTransport{
+
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
---
+++ src/WheeledTransport.java
@@ -0,0 +1,26 @@
+public class WheeledTransport {
+    private  String modelName;
+   private int wheelsCount;
+
+
+    public WheeledTransport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+}
