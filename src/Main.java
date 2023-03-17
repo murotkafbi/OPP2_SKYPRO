ef2829dedd12a6a93c09aff6d301e42a6da34651
unik0 <murotkafbi@gmail.com>
2023-03-17 21:06:53 +0300

    ooow2.text

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("car1", 4);
+        Car car2 = new Car("car2", 4);
+        car.setWheelsCount(4);
+        car2.setWheelsCount(4);
+
+        Truck truck = new Truck("Truck1",6);
+        Truck truck2 = new Truck("Truck2", 8);
+        truck.setWheelsCount(6);
+        truck2.setWheelsCount(8);
+
+        Bicycle bicycle = new Bicycle("Bicycle1",2);
+        Bicycle bicycle2 = new Bicycle("Bicycle2" ,2);
+
+        bicycle.setWheelsCount(2);
+        bicycle2.setWheelsCount(2);
+
+        ServiceStation station = new ServiceStation();
+        station.check(car);
+        station.check(car2);
+        station.check(bicycle);
+        station.check(bicycle2);
+        station.check(truck);
+        station.check(truck2);
     }
 }
